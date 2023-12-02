34649488cb8a8d879cbdec0df680fc93e64d2523
Abdulsalam Abdulmuqsit Olamide <harbdulsarlam@gmail.com>
2023-12-02 05:07:02 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -75,8 +75,8 @@ function App(){
     <div>
         <Router>
 
-      {logIn ? 
-        (
+      {/* {logIn ?  */}
+        {/* ( */}
           <Routes>
               <Route path="/*" element={<LoginPage />} />
               <Route path="/Firstpage" element={<Firstpage />} />
@@ -85,9 +85,9 @@ function App(){
               <Route path='/dashboard' element={<Dashboard />} />
 
           </Routes>
-          ) :
-            null        
-          }
+          {/* ) :
+            null         */}
+          {/* } */}
         </Router>
       </div>
     )
